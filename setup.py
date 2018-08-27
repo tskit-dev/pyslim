a18f30fb57e253702bca9b163465ac8fce8988f5
peter <petrel.harp@gmail.com>
2018-08-26 21:34:47 -0700

    version bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except ImportError:
 
 
 setup(name='pyslim',
-      version='0.0.55',
+      version='0.0.6',
       description=u"Manipulate tree sequences produced by SLiM.",
       long_description=long_description,
       classifiers=[],
