ddcea813bcb98b12b92c9d89ab9b4f3b75e109bc
peter <petrel.harp@gmail.com>
2021-02-24 12:25:06 -0800

    version bump

--- pyslim/_version.py
+++ pyslim/_version.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-pyslim_version = '0.501'
+pyslim_version = '0.600'
 slim_file_version = '0.6'
 # other file versions that require no modification
 compatible_slim_file_versions = ['0.5', '0.6']
