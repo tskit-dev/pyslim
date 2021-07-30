d14302c59ed04f4a89f64f990e691b33be1b6782
Gilia Patterson <32884936+giliapatterson@users.noreply.github.com>
2021-07-30 16:16:24 -0700

    Add functions to compute population size

--- pyslim/spatial.py
+++ pyslim/spatial.py
@@ -10,5 +10,71 @@ from .provenance import *
 from .util import *
 
 
-def population_size():
-    # function goes here
+def in_location_bin(locations, x0, x1, y0, y1):
+    '''
+    Takes the locations of individuals and  tests if each is within [x0, x1), [y0, y1). 
+    Returns a vector the same length as locations with True if they are within the bounds and False otherwise
+    locations is a n x 3 array with rows individuals and [:, 0] the x coordinate and [:, 1] the y coordinate
+    Returns an n x 1 array, TRUE if individual is in the bin
+    Each row corresponds to an individual
+    '''
+    return(np.logical_and(np.logical_and(locations[:,0] < x1, locations[:,0] >= x0),
+               np.logical_and(locations[:,1] < y1, locations[:,1] >= y0)))
+
+def average_time_alive(birth_times, death_times, t0, t1):
+    '''
+    Finds average population size in [t0, t1) using time alive
+    '''
+    # # time steps each individual is alive in [t0, t1)
+    a = np.maximum(0, np.minimum(t1, birth_times) -  np.maximum(t0, death_times))
+    # Average population size
+    return(sum(a)/(t1-t0))
+def population_size(ts, x_bins, y_bins, time_bins):
+    '''
+    Calculates population size in each location bin averaged over each time_bin.   
+    '''
+    
+    # Want to return
+    # [[[px0y0t0, px0y0t1, ...], [px0y1t0, px0y1t1, ...], ...],
+    #  [[px1y0t0, px0y0t1, ...], [px1y1t0, px1y1t1, ...], ...],
+    #  [[px2y0t0, px0y0t1, ...], [px2y1t0, px2y1t1, ...], ...]]
+    # where pxiyjtk is the population size in [x_breaks[i], x_breaks[i + 1]) and [y_breaks[j], x_breaks[j + 1]),
+    # averaged over each time point in [t[k], t[k + 1])
+    # different offsets for different stages, from pyslim.individuals_alive_at code
+    
+    birth_times = ts.individual_times + 1
+    ages = ts.individual_ages
+    death_times = ts.individual_times - ages
+        
+    time_breaks = time_bins
+    x_breaks = x_bins
+    y_breaks = y_bins
+  
+    # Initialize array to store popoluationsize
+    nxbins = len(x_breaks) - 1
+    nybins = len(y_breaks) - 1
+    ntbins = len(time_breaks) - 1
+    popsize = np.empty((nxbins, nybins, ntbins))
+    popsize_validate = np.empty((nxbins, nybins, ntbins))
+    #print(np.shape(popsize))
+
+    # Location, times, and ages of individuals
+    locations = ts.individual_locations
+    times = ts.individual_times
+
+    # Iterate through location bins and time bins
+    for i in np.arange(nxbins):
+        for j in np.arange(nybins):
+            # Endpoints of bins
+            x0, x1 = x_breaks[i], x_breaks[i + 1]
+            y0, y1 = y_breaks[j], y_breaks[j + 1]
+            # Individuals in the location bin
+            in_bin = in_location_bin(locations, x0, x1, y0, y1)
+            for k in np.arange(ntbins):
+                #print(i, j, k)
+                # Endpoints of bins
+                t0, t1 = time_breaks[k], time_breaks[k + 1]
+                #print(x0, x1, y0, y1, t0, t1)
+                # Individuals in the location bin
+                popsize[i, j, k] = average_time_alive(birth_times[in_bin], death_times[in_bin], t0, t1)
+    return(popsize)
--- tests/test_spatial.py
+++ tests/test_spatial.py
@@ -14,13 +14,69 @@ import pyslim
 import tests
 
 class TestPopulationSize(tests.PyslimTestCase):
+    def population_size_simple(self, ts, x_bins, y_bins, time_bins):
+        '''
+        Calculates population size in each location bin averaged over each time_bin.   
+        '''
+        
+        # Want to return
+        # [[[px0y0t0, px0y0t1, ...], [px0y1t0, px0y1t1, ...], ...],
+        #  [[px1y0t0, px0y0t1, ...], [px1y1t0, px1y1t1, ...], ...],
+        #  [[px2y0t0, px0y0t1, ...], [px2y1t0, px2y1t1, ...], ...]]
+        # where pxiyjtk is the population size in [x_breaks[i], x_breaks[i + 1]) and [y_breaks[j], x_breaks[j + 1]),
+        # averaged over each time point in [t[k], t[k + 1])
+        # different offsets for different stages, from pyslim.individuals_alive_at code
+               
+        time_breaks = time_bins
+        x_breaks = x_bins
+        y_breaks = y_bins
+
+        # Initialize array to store popoluationsize
+        nxbins = len(x_breaks) - 1
+        nybins = len(y_breaks) - 1
+        ntbins = len(time_breaks) - 1
+        popsize = np.empty((nxbins, nybins, ntbins))
+        #print(np.shape(popsize))
+
+        # Location, times, and ages of individuals
+        locations = ts.individual_locations
+        times = ts.individual_times
+
+        # Iterate through location bins and time bins
+        for i in np.arange(nxbins):
+            for j in np.arange(nybins):
+                # Endpoints of bins
+                x0, x1 = x_breaks[i], x_breaks[i + 1]
+                y0, y1 = y_breaks[j], y_breaks[j + 1]
+                for k in np.arange(ntbins):
+                    #print(i, j, k)
+                    # Endpoints of bins
+                    t0, t1 = time_breaks[k], time_breaks[k + 1]
+                    alive = 0
+                    for t in np.arange(np.ceil(t0), t1):
+                        for ind_id in ts.individuals_alive_at(t):
+                            ind = ts.individual(ind_id)
+                            if (ind.location[0] < x1 and ind.location[0] >= x0 and ind.location[1] < y1 and ind.location[1] >= y0):
+                                alive += 1/(t1-t0)
+                    popsize[i, j, k] = alive     
+        return(popsize)
+
+    def make_bins(self, ts):
+        for nx in np.arange(1, 20, 10):
+            for ny in np.arange(1, 20, 10):
+                for nt in np.arange(1, 60, 30):
+                    yield [np.linspace(0, round(max(ts.individual_locations[:,0])), nx + 1), 
+                           np.linspace(0, round(max(ts.individual_locations[:,1])), ny + 1),
+                           np.round(np.linspace(0, max(ts.individual_times), nt + 1))]  
 
     def verify(self, ts):
-        # as computed by pyslim
-        popsize0 = pyslim.population_size(ts)
-        # as computed by testing code
-        popsize1 = # compute in a simple way here
-        assert np.array_equal(popsize0, popsize1)
+        for bins in make_bins(ts):
+            x_bins, y_bins, time_bins = bins
+            # as computed by pyslim
+            popsize0 = population_size(ts, x_bins, y_bins, time_bins)
+            # as computed in a simple way
+            popsize1 = population_size_simple(ts, x_bins, y_bins, time_bins)
+            assert(np.allclose(popsize1, popsize0))
 
     def test_population_size_errors(self):
         # test that it fails appropriately on bad input
