19760de8b9d334ce2d5ad4e2a3bdaec129ab44e9
andrewkern <adkern@uoregon.edu>
2022-01-31 22:07:23 -0800

    moved example notebook, added reps magic

--- pyslim_magic/pyslim_magic.py
+++ pyslim_magic/pyslim_magic.py
@@ -23,6 +23,33 @@ class PySlimMagic(Magics):
                     break
                 else:
                     count += 1
-        return pd.read_csv(logfile, skiprows=count)
+        df = pd.read_csv(logfile, skiprows=count)
+        df = df.set_index('generation')
+        return df
 
+    @cell_magic
+    def slim_stats_reps(self, reps, cell):
+        script = cell
+        n = int(reps)
+        aList = []
+        for i in range(n):
+            logfile = "tmp.log"
+            os.system("echo '" + script + "' | slim > " + logfile)
+            # deal with ugliness of slim output and cut off
+            # the top few header lines
+            pattern = "generation,"
+            count = 0
+            with open(logfile, "r") as f:
+                for line in f:
+                    if pattern in line:
+                        break
+                    else:
+                        count += 1
+            df = pd.read_csv(logfile, skiprows=count)
+            df = df.set_index('generation')
+            df.rename(columns={'H':'H'+str(i)}, inplace=True)
+            aList.append(df)
+            dff = pd.concat(aList, axis=1, join="inner")
+        return dff
+        
 
