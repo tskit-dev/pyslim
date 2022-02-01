d77779fa93c8035b8d1f60118fa0b91bf76df4cb
andrewkern <adkern@uoregon.edu>
2022-02-01 11:39:14 -0800

    further progress on magic

--- pyslim_magic/pyslim_magic.py
+++ pyslim_magic/pyslim_magic.py
@@ -52,4 +52,28 @@ class PySlimMagic(Magics):
             dff = pd.concat(aList, axis=1, join="inner")
         return dff
         
+    @cell_magic
+    def slim_stats_reps_stack(self, reps, cell):
+        script = cell
+        n = int(reps)
+        aList = []
+        for i in range(n):
+            logfile = "tmp.log"
+            os.system("echo '" + script + "' | slim > " + logfile)
+            # deal with ugliness of slim output and cut off
+            # the top few header lines
+            pattern = "generation,"
+            count = 0
+            with open(logfile, "r") as f:
+                for line in f:
+                    if pattern in line:
+                        break
+                    else:
+                        count += 1
+            df = pd.read_csv(logfile, skiprows=count)
+            df = df.set_index('generation')
+            aList.append(df)
+            dff = pd.concat(aList)
+        return dff
+        
 
