503f13417e05fcf03bb5f9ae272047c858c914b6
Jerome Kelleher <jk@well.ox.ac.uk>
2026-03-09 10:01:01 +0000

    Switch to setuptools_scm for version management

--- pyslim/_version.py
+++ pyslim/_version.py
@@ -1,4 +1,10 @@
-pyslim_version = "1.1.2.dev0"
+try:
+    from importlib.metadata import version as _get_version
+
+    pyslim_version = _get_version("pyslim")
+except Exception:
+    pyslim_version = "unknown"
+
 slim_file_version = "0.9"
 # other file versions that require no modification
 compatible_slim_file_versions = ["0.9"]
